5d3359874058b0ecf875bb5e7e132c17b7a574e5
박상원 <park9707>
2022-11-29 16:33:36 +0900

    문자열 압축

---
+++ programmers/문자열 압축/문자열 압축.py
@@ -0,0 +1,31 @@
+def solution(s):
+    answer = len(s)
+    
+    for i in range(1, len(s)//2+1):
+        cnt = 1
+        b = ''
+        tmp = s[:i]
+        
+        for j in range(i, len(s), i):
+            if tmp==s[j:j+i]:
+                cnt+=1
+            else:
+                if cnt == 1:
+                    b += tmp
+                else:
+                    b += str(cnt) + tmp
+                cnt = 1
+                tmp = s[j:j+i]
+        
+        if cnt == 1:
+            b += tmp
+        else:
+            b += str(cnt) + tmp
+        
+        answer = min(answer, len(b))
+    
+    return answer
+
+s=input()
+
+print(solution(s))
