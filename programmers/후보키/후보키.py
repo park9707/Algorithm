0274f2a8f6327356f152ea060124a4eb00483b45
박상원 <park9707>
2023-01-23 16:38:28 +0900

    후보키

--- programmers/후보키/후보키.py
+++ programmers/후보키/후보키.py
@@ -16,5 +16,3 @@ def solution(relation):
                 arr.append(i)
 
     return len(arr)
-
-print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
