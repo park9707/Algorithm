12dad8b298079e35601e23a25bb0bd1acfef2ed9
박상원 <park9707>
2023-01-26 14:32:27 +0900

    숫자 블록

---
+++ programmers/숫자 블록/숫자 블록.py
@@ -0,0 +1,13 @@
+def solution(begin, end):
+    answer = []
+    if begin == 1:
+        answer.append(0)
+        begin += 1
+    for i in range(begin, end+1):
+        a = 1
+        for j in range(2, int(i**0.5)+1):
+            if i % j == 0 and i//j <= 10000000:
+                a = i//j
+                break
+        answer.append(a)
+    return answer
