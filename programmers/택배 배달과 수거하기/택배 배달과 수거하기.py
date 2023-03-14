09164993b4d2bbe781b29801b88ce3a422452f50
박상원 <sodkeldla@gmail.com>
2023-03-14 13:14:08 +0900

    택배 배달과 수거하기

---
+++ programmers/택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -0,0 +1,16 @@
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    d = p = 0
+    for i in range(n - 1, -1, -1):  # 가장 먼 곳 부터
+        d += deliveries[i]  # i 번째의 배달 량
+        p += pickups[i]  # i 번째의 수거 량
+
+        while d > 0 or p > 0:  # 배달 또는 수거 해야 할 것이 있다면 무조건 와야함, 음수라면 가장 먼 곳을 가는 김에 배달 또는 수거 가능
+            d -= cap  # 최대 적재량 만큼 빼기
+            p -= cap  # 최대 적재량 만큼 빼기
+            answer += (i+1) * 2  # 왕복을 해야 하므로 가장 먼 거리 * 2
+        # 첫 배달 또는 수거 이후 양수가 되어야 와야 될 지역, 음수라면 먼 곳 왕복 시 들러서 수행 가능
+    return answer
+
+print(solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]))
+
