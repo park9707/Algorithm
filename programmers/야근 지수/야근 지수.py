bbd65b0f069496e706018b0e5c8ca375c69b6ca3
박상원 <sodkeldla@gmail.com>
2023-03-22 12:32:38 +0900

    야근 지수

---
+++ programmers/야근 지수/야근 지수.py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(n, works):
+    if n >= sum(works):
+        return 0
+
+    heap = [-i for i in works]
+    heapq.heapify(heap)
+    for _ in range(n):
+        num = heapq.heappop(heap)
+        heapq.heappush(heap, num+1)
+
+    return sum(m**2 for m in heap)
+
+print(solution(4, [4,3,3]))
