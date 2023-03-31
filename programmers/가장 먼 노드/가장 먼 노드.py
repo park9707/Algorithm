66d02c01ce2e96cef7d3de16e92ea9124beefdd6
박상원 <sodkeldla@gmail.com>
2023-03-31 11:50:10 +0900

    가장 먼 거리

---
+++ programmers/가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,23 @@
+from collections import deque, defaultdict
+
+
+def solution(n, edge):
+    maps = [[] for _ in range(n + 1)]
+    arr = defaultdict(list)
+    for a, b in edge:
+        maps[a].append(b)
+        maps[b].append(a)
+
+    visited = [-1] * (n + 1)
+    visited[1] = 0
+    q = deque([1])
+
+    while q:
+        num = q.popleft()
+        for i in maps[num]:
+            if visited[i] == -1:
+                q.append(i)
+                visited[i] = visited[num] + 1
+                arr[visited[i]].append(i)
+
+    return len(arr[len(arr)])
