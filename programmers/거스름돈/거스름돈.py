d127889704a30fdb09673e0db53070fdeed58aa6
박상원 <sodkeldla@gmail.com>
2023-05-04 12:24:22 +0900

    거스름돈

---
+++ programmers/거스름돈/거스름돈.py
@@ -0,0 +1,9 @@
+def solution(n, money):
+    answer = [1] + [0] * n
+
+    for m in money:
+        for i in range(m, n+1):
+            answer[i] += answer[i-m]
+    return answer[n]
+
+print(solution(5, [1, 2, 5]))
