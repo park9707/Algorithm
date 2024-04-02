f7668af36a3fd7bae967c7a9379a0dd76609bc66
park9707 <sodkeldla@gmail.com>
2024-04-02 13:51:15 +0900

    타임머신

--- baekcjoon/타임머신(11657)/타임머신.py
+++ baekcjoon/타임머신(11657)/타임머신.py
@@ -1,24 +1,31 @@
 import sys
+
 input = sys.stdin.readline
 
 
 def bellman_ford(start):
     node[start] = 0
-    for i in range(N-1):
-        for j in range(M):
-            cur_node, next_node, cost = wire[j]
-            node[next_node] = min(node[next_node], node[cur_node] + cost)
+    for _ in range(N - 1):
+        for cur_node in range(1, N + 1):
+            if node[cur_node] == float("inf"):
+                continue
 
-    for j in range(M):
-        cur_node, next_node, cost = wire[j]
-        if node[next_node] > node[cur_node] + cost:
-            return False
+            for next_node, cost in wire[cur_node]:
+                node[next_node] = min(node[next_node], node[cur_node] + cost)
+
+    for cur_node in range(1, N + 1):
+        for next_node, cost in wire[cur_node]:
+            if node[next_node] > node[cur_node] + cost:
+                return False
     return True
 
 
 N, M = map(int, input().split())
 node = [float("inf")] * (N + 1)
-wire = [list(map(int, input().split())) for _ in range(M)]
+wire = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    wire[a].append([b, c])
 
 if not bellman_ford(1):
     print(-1)
