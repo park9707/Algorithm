356bd82262f4264e922f58f83de2298dacb4258b
park9707 <sodkeldla@gmail.com>
2024-04-05 13:25:59 +0900

    행렬 곱셈 순서

---
+++ baekcjoon/행렬 곱셈 순서(11409)/행렬 곱셈 순서.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * N for _ in range(N)]
+
+for i in range(1, N):
+    for j in range(N - i):
+        dp[j][j + i] = float("inf")
+        for k in range(j, j + i):
+            dp[j][j + i] = min(dp[j][j + i], dp[j][k] + dp[k + 1][j + i] + arr[j][0] * arr[k][1] * arr[j + i][1])
+
+print(dp[0][N - 1])
