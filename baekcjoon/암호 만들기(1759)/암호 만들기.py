220dd143a2be01a762148b3318d1ade61b09ed3d
park9707 <sodkeldla@gmail.com>
2024-05-07 18:17:08 +0900

    암호 만들기

---
+++ baekcjoon/암호 만들기(1759)/암호 만들기.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+
+def back_tracking(cnt, idx):
+    if cnt == l:
+        vo, co = 0, 0
+
+        for i in range(l):
+            if answer[i] in ['a', 'e', 'i', 'o', 'u']:
+                vo += 1
+            else:
+                co += 1
+
+        if vo >= 1 and co >= 2:
+            print(''.join(answer))
+        return
+
+    for i in range(idx, c):
+        answer.append(words[i])
+        back_tracking(cnt + 1, i + 1)
+        answer.pop()
+
+
+l, c = map(int, input().split())
+words = sorted(list(map(str, input().split())))
+answer = []
+back_tracking(0, 0)
