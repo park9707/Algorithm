8ab22d577819f0c344ba6a2608ea9d095f7b8fe5
park9707 <sodkeldla@gmail.com>
2025-07-18 15:26:53 +0900

    운동

---
+++ baekcjoon/운동(1956)/운동.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+v, e = map(int, input().split())
+dist = [[float('inf')] * (v + 1) for _ in range(v + 1)]
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    dist[a][b] = c
+
+for k in range(1, v + 1):
+    for i in range(1, v + 1):
+        for j in range(1, v + 1):
+            if dist[i][j] > dist[i][k] + dist[k][j]:
+                dist[i][j] = dist[i][k] + dist[k][j]
+
+ans = float('inf')
+for i in range(1, v + 1):
+    ans = min(ans, dist[i][i])
+
+print(ans if ans != float('inf') else -1)
