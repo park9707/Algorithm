551790139152ba669507df77492e14fff4441975
park9707 <sodkeldla@gmail.com>
2025-11-27 21:11:09 +0900

    읽씹 멈춰!

---
+++ baekcjoon/읽씹 멈춰!(27377)/읽씹 멈춰!.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
+    s, t = map(int, input().split())
+    ans = 0
+    while n > 0:
+        if n % 2 == 1:
+            n -= 1
+            ans += s
+        else:
+            n //= 2
+            ans += min(n * s, t)
+    print(ans)
