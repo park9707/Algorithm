7433b16eb4029295dc3935e63f63af2949609b5c
park9707 <sodkeldla@gmail.com>
2026-02-12 13:56:01 +0900

    타일 채우기

---
+++ baekcjoon/타일 채우기(2133)/타일 채우기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * 31
+dp[0], dp[2] = 1, 3
+
+for i in range(4, n + 1, 2):
+    dp[i] = dp[i - 2] * 4 - dp[i - 4]
+
+print(dp[n])
